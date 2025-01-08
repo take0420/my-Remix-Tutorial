4faafcb3e0a95e5d60488e97326b81ca7d81ae9a
take0420 <zakisen81@gmail.com>
2025-01-08 18:33:03 +0900

    update: 同期的な検索機能の追加

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from '@remix-run/react';
 import { createEmptyContact, getContacts } from './data';
 import {
@@ -17,9 +18,10 @@ import {
   json,
   redirect,
 } from '@remix-run/node';
+import { useEffect } from 'react';
 import appStylesHref from './app.css?url';
 
-export const acton = async () => {
+export const action = async () => {
   const contact = await createEmptyContact();
   return redirect(`/contacts/${contact.id}/edit`);
 };
@@ -32,12 +34,26 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get('q');
   const contacts = await getContacts(q);
-  return json({ contacts });
+  if (!contacts) {
+    throw new Response('No contacts found', { status: 404 });
+  }
+  return json({ contacts, q });
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
+
+  useEffect(() => {
+    const searchField = document.getElementById('q');
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || '';
+    }
+  }, [q]);
 
   return (
     <html lang='en'>
@@ -49,7 +65,9 @@ export default function App() {
       </head>
       <body>
         <div
-          className={navigation.state === 'loading' ? 'loading' : ''}
+          className={
+            navigation.state === 'loading' && !searching ? 'loading' : ''
+          }
           id='detail'
         >
           <Outlet />
@@ -57,18 +75,26 @@ export default function App() {
         <div id='sidebar'>
           <h1>Remix Contacts</h1>
           <div>
-            <Form id='search-form' role='search'>
+            <Form
+              id='search-form'
+              onChange={(event) => submit(event.currentTarget)}
+              role='search'
+            >
               <input
-                id='q'
                 aria-label='Search contacts'
+                className={searching ? 'loading' : ''}
+                defaultValue={q || ''}
+                id='q'
+                name='q'
                 placeholder='Search'
                 type='search'
-                name='q'
               />
-              <div id='search-spinner' aria-hidden hidden={true} />
+              <div aria-hidden hidden={!searching} id='search-spinner' />
             </Form>
             <Form method='post'>
-              <button type='submit'>New</button>
+              <button type='submit' aria-label='Create new contact'>
+                New
+              </button>
             </Form>
           </div>
           <nav>
@@ -78,20 +104,18 @@ export default function App() {
                   <li key={contact.id}>
                     <NavLink
                       className={({ isActive, isPending }) =>
-                        isActive ? 'acitve' : isPending ? 'pending' : ''
+                        isActive ? 'active' : isPending ? 'pending' : ''
                       }
                       to={`contacts/${contact.id}`}
                     >
-                      <Link to={`contacts/${contact.id}`}>
-                        {contact.first || contact.last ? (
-                          <>
-                            {contact.first} {contact.last}
-                          </>
-                        ) : (
-                          <i>No Name</i>
-                        )}{' '}
-                        {contact.favorite ? <span>*</span> : null}
-                      </Link>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{' '}
+                      {contact.favorite ? <span>*</span> : null}
                     </NavLink>
                   </li>
                 ))}
